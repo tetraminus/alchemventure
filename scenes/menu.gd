2b9dde012ee75996edeb1d839c1112beb7752af7
tetraminus <54343976+tetraminus@users.noreply.github.com>
2021-09-21 15:17:40 -0700

    bully

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -2,8 +2,8 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+ pass
 
 func _on_Button_button_down():
 	print("switch!")
-	get_tree().change_scene("res://scenes/test.tscn")
+	#get_tree().change_scene("res://scenes/test.tscn")
