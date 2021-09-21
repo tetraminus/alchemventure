db9fe2e2d59736b1ebc078b0f8f05a8bedb78da7
tetraminus <54343976+tetraminus@users.noreply.github.com>
2021-09-21 14:59:13 -0700

    add

---
+++ scenes/menu.gd
@@ -0,0 +1,9 @@
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func _on_Button_button_down():
+	print("switch!")
+	get_tree().change_scene("res://scenes/test.tscn")
