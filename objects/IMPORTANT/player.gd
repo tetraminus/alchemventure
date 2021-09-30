849c267095cf25b5e259a8264e57fe5c08cdbd09
tetraminus <54343976+tetraminus@users.noreply.github.com>
2021-09-29 19:10:50 -0700

    player image

--- objects/IMPORTANT/player.gd
+++ objects/IMPORTANT/player.gd
@@ -30,9 +30,9 @@ func _physics_process(delta):
 		
 	if Input.is_action_pressed("jump"):
 		if is_on_floor():
-			velocity.y += (jump_speed/5)
+			velocity.y = (jump_speed/5)
 			jumping = true
-	if not Input.is_action_pressed("jump") or jump_frame_count >= 10:
+	if not Input.is_action_pressed("jump") or jump_frame_count > 10:
 		jumping = false
 		jump_frame_count = 1
 		
