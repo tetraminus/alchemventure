02f5086d5e94ce546a0e2f80cb38daeb35418348
tetraminus <54343976+tetraminus@users.noreply.github.com>
2021-10-19 08:34:49 -0700

    testing time'

--- objects/IMPORTANT/potions/potion.gd
+++ objects/IMPORTANT/potions/potion.gd
@@ -2,7 +2,8 @@ extends Area2D
 
 const textures = [
 	preload("res://tex/POTIONS/RND_POTION.png"),
-	preload("res://tex/POTIONS/SQR_POTION.png")
+	preload("res://tex/POTIONS/SQR_POTION.png"),
+	preload("res://tex/POTIONS/POLY_POTION.png")
 ]
 export (Array) var effects = []
 export (int) var Power = 1
@@ -10,8 +11,9 @@ onready var pot_Sprite = self.get_node("Sprite")
 
 
 func _ready():
-	print(rand_range(0,textures.size()))
 	randomize()
-	pot_Sprite.set_texture(textures[int(round(rand_range(0,textures.size()-1)))])
+	var cur_tex = int(round(rand_range(0,textures.size()-1)))
+	print(textures.size()-1)
+	pot_Sprite.set_texture(textures[cur_tex])
 	print(effects)
 	print(Power)
